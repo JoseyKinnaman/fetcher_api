ef1ab45f5da6cf0f0ca544c8950cd8606c946731
Jozy Kinnaman <jozypants@gmail.com>
2020-07-17 15:04:49 -0700

    test seed db and add Module and controller info.

---
+++ app/controllers/names_controller.rb
@@ -0,0 +1,36 @@
+class NamesController < ApplicationController
+
+  def index
+    @names = Name.all
+    json_response(@names)
+  end
+
+  def show
+    @name = Name.find(params[:id])
+    json_response(@name)
+  end
+
+  def create
+    @name = Name.create(name_params)
+    json_response(@name)
+  end
+
+  def update
+    @name = Name.find(params[:id])
+    @name.update(name_params)
+  end
+
+  def destroy
+    @name = Name.find(params[:id])
+    @name.destroy
+  end
+
+  private
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
+
+  def name_params
+    params.permit(:name, :categoy, :male, :female, :comments)
+  end
+end
